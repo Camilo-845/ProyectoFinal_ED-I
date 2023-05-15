50c66aba17b3e664821f500a144200e0da4fd8b1
Eddie <edavidmanotas@gmail.com>
2023-05-15 18:33:15 -0500

    Creación de estructuras y main

---
+++ (Main)Presentacion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "estructuras.h"
+#include <wchar.h>
+#include <locale.h>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "");
+    
+    return 0;
+}
+
---
+++ estructuras.h
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Cliente{
+	string cedula;
+	string nombre;
+	string direccion;
+	int numero;
+	string email;
+	Cliente *sig;
+};
+
+struct Fecha{
+	int dia;
+	int mes;
+	int aniho;
+};
+
+struct Producto{
+	string codigo;
+	string descripcion;
+	int valorUnitario;
+	int stock;
+};
+
+struct Detalle{
+	string producto;
+	int cantidad;
+	int precio;
+	int subtotal;
+	Detalle *sig;
+};
+
+struct Factura{
+	unsigned long int numero;
+	Fecha fechaVenta;
+	Cliente *cliente;
+	Detalle *cabezaDetalle;
+	Factura *sig;
+};
+
+
